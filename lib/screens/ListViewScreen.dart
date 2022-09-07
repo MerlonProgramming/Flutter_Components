02b3b193b1d64a8b8f784d2bc71d4663022416dc
Marlon <msdiazl@bancodeoccidente.com.co>
2022-09-06 19:54:03 -0500

    The project is created for the management of different components in flutter, besides creating the first list and having an array with this ListTile list.

---
+++ lib/screens/ListViewScreen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ListViewScreen extends StatelessWidget {
+  final optionsMenu = const [
+    '🧑🏿‍🚀 Megaman',
+    '🎖 Metal Gear',
+    '💥 Super Smash',
+    '🐲 Final Fantasy'
+  ];
+
+  const ListViewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('ListView tipo 1'),
+        ),
+        body: ListView(
+          children: [
+            ...optionsMenu
+                .map((game) => ListTile(
+                      title: Text(game),
+                      trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                    ))
+                .toList()
+          ],
+        ));
+  }
+}
