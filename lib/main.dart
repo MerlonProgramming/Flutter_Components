da76c91f85e0e0ef23a6a765cc1076b0ff2f4f95
Marlon <msdiazl@bancodeoccidente.com.co>
2022-09-06 20:06:55 -0500

    The second work list is created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 //imports
-import 'package:flutter_components/screens/ListViewScreen.dart';
+//import 'package:flutter_components/screens/ListViewScreen.dart';
+import 'package:flutter_components/screens/ListView2Screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,6 +13,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
-        home: ListViewScreen());
+        home: ListView2Screen());
   }
 }
